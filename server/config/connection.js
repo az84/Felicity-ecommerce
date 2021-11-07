6bfa65fb4f8f39ab78d3f303c78cee4b40c4d27a
azaidi <zaidialina1@gmail.com>
2021-11-07 01:28:08 -0400

    trying to connect app to mongodb atlas

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/Felicity',
+  process.env.MONGODB_URI || 'mongodb://localhost/Ecommerce',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
